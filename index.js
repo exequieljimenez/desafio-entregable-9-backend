6fe33b6f63fa4e24dd8fcd082ea99e895199feb1
exequieljimenez <exequieljimenez@gmail.com>
2023-02-15 12:14:58 -0300

    desafio 9 completo

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const faker = require('faker')
 
 const {Server: HttpServer} = require('http');
-const {Server: Socket} = require('socket.io')
+const {Server: Socket} = require('socket.io');
+
+const ContenedorArchivo = require('./contenedores/ContenedorArchivo')
 
 const app = express()
 const httpServer = new HttpServer(app)
 const io = new Socket(httpServer)
 
-const producto1 = {nombre: faker.commerce.product(), precio: faker.commerce.price(), imagen: faker.image.imageUrl()}
+const contenedorArchivo = new ContenedorArchivo('./DB/mensajes.json')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 function productosRandom() {
     return {
@@ -28,16 +34,18 @@ function crearProductos() {
 
 io.on('connection', socket => {
     socket.emit('productos', crearProductos())
-    
-})
+    const mensajes = contenedorArchivo.listarAll()
+    socket.emit('mensajes', mensajes)
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
+    socket.on('message', data => {
+        contenedorArchivo.agregarMensaje(data)
+
+        io.sockets.emit('mensajes', contenedorArchivo.listarAll())
+    })
+})
 
 const PORT = 8080
 const connectedServer = httpServer.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+    console.log(`Servidor http esuchando en el puerto ${connectedServer.address().port}`)
 })
-
-connectedServer.on('error', error => console.log(`Error en el servidor ${error}`))
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
